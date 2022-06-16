f2390bffe74989fffb40bacd1edc665bebc32310
Dimas Kurniawan <ichbindimas@gmail.com>
2022-06-16 10:49:56 +0800

    add graphql for get all Article

--- section 1/orchestrator/app.js
+++ section 1/orchestrator/app.js
@@ -1,8 +1,42 @@
 const { gql, ApolloServer } = require("apollo-server");
-const typeDefs = require("./typeDefs");
 const PORT = process.env.PORT || 4001;
 const axios = require("axios");
 
 const mainUrl = "http://localhost:4000";
 
 const typeDefs = gql`
+type Article {
+    _id: ID
+    title: String
+    content: String
+    images: String
+    author: String
+    } 
+    type Query {
+        articles: [Article]
+    }
+`
+
+const resolvers = {
+    Query: {
+        articles: async () => {
+            try {
+                const response = await axios.get(`${mainUrl}/articles`);
+                return response.data;
+            } catch (error) {
+                return error
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true,
+});
+
+server.listen(PORT).then(({ url }) => {
+    console.log(`running => ${url}`);
+});
